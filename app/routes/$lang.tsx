1a60494a0d4f10512f9935b1795f6d15102a8461
cychien <xyz030206@gmail.com>
2022-08-20 11:06:11 +0800

    feat: improve i18n routing

--- app/routes/$lang.tsx
+++ app/routes/$lang.tsx
@@ -1,25 +1,39 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+import parser from "accept-language-parser";
 
-import { detectLanguageOnServer } from "~/utils/i18n";
+import { SUPPORTED_LANGUAGES } from "~/utils/i18n/constants";
 
-export const loader: LoaderFunction = ({ request }) => {
-  const url = new URL(request.url);
+export const loader: LoaderFunction = ({ request, params }) => {
+  const lang = params.lang;
 
-  const path = url.pathname;
-  const pathParts = path.split("/");
-  const isIndexPage = pathParts.length <= 2;
-
-  const lng = detectLanguageOnServer(request);
-
-  if (lng === pathParts[1]) {
-    return null;
+  if (!lang) {
+    throw new Response("Not Found", {
+      status: 404,
+    });
   }
 
-  if (isIndexPage) return redirect(`/${lng}`);
+  const url = new URL(request.url);
+  // The pathname will be like: /en-US/
+  const pathname = url.pathname;
+  const pathnameWithoutLang = pathname.split("/").slice(2).join("/");
 
-  return redirect(`/${lng}${pathParts.slice(2).join("/")}`);
+  const supportedLanguage = parser.pick(SUPPORTED_LANGUAGES, lang, {
+    loose: true,
+  });
+
+  if (!supportedLanguage) {
+    throw new Response("Not Found", {
+      status: 404,
+    });
+  }
+
+  if (supportedLanguage !== lang) {
+    return redirect(`/${supportedLanguage}/${pathnameWithoutLang}`);
+  }
+
+  return null;
 };
 
 export default function LangLayout() {
