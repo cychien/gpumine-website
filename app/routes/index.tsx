302b693aecfbe7699b95509ae6c4e56329b20194
cychien <xyz030206@gmail.com>
2022-08-24 21:14:44 +0800

    chore: change let to const

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ import { redirect } from '@remix-run/node'
 import { detectLanguageOnServer } from '~/utils/i18n'
 
 export const loader: LoaderFunction = ({ request }) => {
-  let lng = detectLanguageOnServer(request)
+  const lng = detectLanguageOnServer(request)
 
   return redirect(`/${lng}`)
 }
