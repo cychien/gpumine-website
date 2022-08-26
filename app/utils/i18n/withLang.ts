cde3d52b9a81469fef5b22fc9d4d25056904a34b
cychien <xyz030206@gmail.com>
2022-08-26 23:22:09 +0800

    chore: remove trailing slash in url

--- app/utils/i18n/withLang.ts
+++ app/utils/i18n/withLang.ts
@@ -3,7 +3,7 @@
 function withLang(pathname: string = '', language: string = 'en-US') {
   return pathname.startsWith('/')
     ? `/${language}${pathname}`
-    : `/${language}/${pathname}`
+    : `/${language}${pathname ? '/' + pathname : ''}`
 }
 
 export default withLang
