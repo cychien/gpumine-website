57b7b895135e32a2f8fb3a19c66519a218db83cc
cychien <xyz030206@gmail.com>
2022-08-27 09:54:36 +0800

    refactor: home page stats

---
+++ app/components/home/Stats/Stats.tsx
@@ -0,0 +1,73 @@
+import { useTranslation } from 'react-i18next'
+
+import commonIcons from '~/assets/icons/common.svg'
+
+export default function Stats() {
+  return (
+    <div className="bg-surface w-full max-w-[782px] rounded-2xl px-6 py-4 shadow-[0_4px_8px_0_rgba(189,206,252,0.2)] sm:py-6">
+      <div className="flex items-center justify-between">
+        <CurrencyType />
+
+        <div className="flex-1">
+          <StatsNumbers />
+        </div>
+
+        <button type="button" className="p-2">
+          <svg className="h-[10px] w-[20px] text-lightGray sm:h-[12px] sm:w-[24px]">
+            <use href={`${commonIcons}#arrow-down`} />
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function CurrencyType() {
+  return (
+    <div className="flex flex-col items-center text-primary-500 sm:flex-row">
+      <svg className="h-[36px] w-[36px] sm:h-[60px] sm:w-[60px]">
+        <use href={`${commonIcons}#eth`} />
+      </svg>
+      <div className="mt-[-4px] text-sm font-bold sm:mt-0 sm:text-xl">ETH</div>
+    </div>
+  )
+}
+
+function StatsNumbers() {
+  const { t } = useTranslation()
+
+  return (
+    <div className="flex justify-center space-x-6 sm:items-start sm:space-x-16">
+      <div className="hidden flex-col items-center sm:flex">
+        <div className="mb-1 text-xs text-lightGray">
+          {t('common.whole-computing-power')}
+        </div>
+        <div className="font-bold text-primary-500 sm:text-xl">1200</div>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <div className="mb-1 hidden text-xs text-lightGray sm:block">
+          {t('common.pool-computing-power')}
+        </div>
+        <div className="mb-1 text-sm font-bold text-darkGray sm:text-xl">
+          1.58
+        </div>
+        <div className="text-xs text-lightGray">TH</div>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="mb-1 hidden text-xs text-lightGray sm:block">
+          {t('common.daily-earning')}
+        </div>
+        <div className="mb-1 text-sm font-bold text-darkGray sm:text-xl">
+          0.00746
+        </div>
+        <div className="text-xs text-lightGray">100MH</div>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="mb-1 hidden text-xs text-lightGray sm:block">USD</div>
+        <div className="text-sm font-bold text-darkGray sm:text-xl">2.238</div>
+        <div className="text-xs text-lightGray">USD</div>
+      </div>
+    </div>
+  )
+}
