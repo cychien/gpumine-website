b70bfc49d56dab32b2a9dd736a8c2f7e962a9588
cychien <xyz030206@gmail.com>
2022-09-16 00:08:50 +0800

    chore: temporarily remove announcement

--- app/routes/$lang/index.tsx
+++ app/routes/$lang/index.tsx
@@ -1,23 +1,23 @@
 import { useTranslation } from 'react-i18next'
 
-import Announcement from '~/components/Announcement'
+// import Announcement from '~/components/Announcement'
 import CurrencyDropdown from '~/components/home/CurrencyDropdown'
 import Stats from '~/components/home/Stats'
 import Title from '~/components/Title'
-import useLocalStorage from '~/utils/hooks/useLocalStorage'
+// import useLocalStorage from '~/utils/hooks/useLocalStorage'
 
 export default function Index() {
   const { t } = useTranslation()
 
-  const [announcements, setAnnouncements] = useLocalStorage(
-    'announcements',
-    { demo: false },
-    { demo: true }
-  )
+  // const [announcements, setAnnouncements] = useLocalStorage(
+  //   'announcements',
+  //   { demo: false },
+  //   { demo: true }
+  // )
 
   return (
     <main className="flex flex-col items-center px-3 pt-4 pb-8 sm:pb-16">
-      {!!announcements?.demo && (
+      {/* {!!announcements?.demo && (
         <div className="w-full max-w-[964px]">
           <Announcement
             onClose={() => {
@@ -25,7 +25,7 @@ export default function Index() {
             }}
           />
         </div>
-      )}
+      )} */}
 
       <section className="mt-5 flex w-full max-w-[782px] flex-col space-y-4 sm:mt-12 sm:space-y-7">
         <div className="relative">
