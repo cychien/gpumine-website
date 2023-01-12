111f058d90f8a9043a4091a531c304a4406dc4ab
cychien <xyz030206@gmail.com>
2023-01-12 23:30:16 +0800

    feat: add mode toggle

--- app/components/SegmentedControl/SegmentedControl.tsx
+++ app/components/SegmentedControl/SegmentedControl.tsx
@@ -9,22 +9,35 @@ type SegmentedControlProps = {
   options: Option[]
   value: string
   onChange: (newValue: string) => void
+  className?: string
+  controlClassName?: string
+  activeControlClassName?: string
 }
 
 function SegmentedControl({
   options,
   value: currentValue,
   onChange,
+  className,
+  controlClassName,
+  activeControlClassName,
 }: SegmentedControlProps) {
   return (
-    <div className="inline-flex rounded-full border border-primary-200 p-1">
+    <div
+      className={cx(
+        'inline-flex rounded-full border border-primary-200 p-1',
+        className
+      )}
+    >
       {options.map(({ label, value }) => (
         <button
           key={value}
           className={cx(
             'flex cursor-pointer items-center justify-center rounded-full py-1 px-3 text-xs font-medium text-primary-600',
+            controlClassName,
             {
-              'bg-primary-600 !text-white': currentValue === value,
+              [`bg-primary-600 !text-white ${activeControlClassName}`]:
+                currentValue === value,
             }
           )}
           onClick={() => {
--- app/routes/$lang/worker/$address.tsx
+++ app/routes/$lang/worker/$address.tsx
@@ -1,10 +1,11 @@
 import type { LoaderFunction } from '@remix-run/node'
 import { useParams } from '@remix-run/react'
+import * as React from 'react'
 import { useTranslation } from 'react-i18next'
 
 import EstimatedIncomeReport from '~/components/miner/EstimatedIncomeReport'
 import WithdrawReport from '~/components/miner/WithdrawReport'
-import Title from '~/components/Title'
+import SegmentedControl from '~/components/SegmentedControl'
 import AccountingReport from '~/components/worker/AccountingReport'
 import AddressCard from '~/components/worker/AddressCard'
 import BalanceCard from '~/components/worker/BalanceCard'
@@ -26,14 +27,28 @@ export const loader: LoaderFunction = ({ params }) => {
 }
 
 export default function Address() {
+  const [mode, setMode] = React.useState<string>('lite')
   const { address } = useParams()
   const { t } = useTranslation()
 
   return (
     <main className="mx-auto flex w-full max-w-[964px] flex-col space-y-16 px-3 pt-4 pb-8 sm:pb-16">
       <section className="mt-5 flex flex-col space-y-3 sm:mt-7 lg:space-y-5">
-        <div className="relative mb-2 self-start sm:self-stretch lg:mb-0">
-          <Title>{t('common.mining-status')}</Title>
+        <div className="relative mb-2 lg:mb-0">
+          <h1 className="text-xl font-bold sm:text-center">
+            {t('common.mining-status')}
+          </h1>
+          <SegmentedControl
+            className="absolute right-0 top-1/2 -translate-y-1/2 border-[2px] border-white p-0.5"
+            controlClassName="py-1.5 px-5 text-darkGray"
+            activeControlClassName="bg-gradient-to-r from-primary-600 to-primary-400"
+            options={[
+              { label: t('common.lite-mode'), value: 'lite' },
+              { label: t('common.advanced-mode'), value: 'advanced' },
+            ]}
+            value={mode}
+            onChange={setMode}
+          />
         </div>
 
         <div className="flex flex-col space-y-3 sm:space-y-5">
